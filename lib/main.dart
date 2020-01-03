1ed7f7123bfa8649f15ad52c2e23e07138e63930
Anoop.P.A <sripaanoop@gmail.com>
2020-01-03 13:58:11 +0530

    Add login page layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:samayakuri2/screens/home_screen.dart';
+import 'package:samayakuri2/screens/login/login.dart';
 import 'package:samayakuri2/screens/profile.dart';
 import 'package:samayakuri2/screens/splash_screen.dart';
 import 'package:samayakuri2/screens/time_calc.dart';
@@ -7,13 +8,14 @@ import 'package:samayakuri2/screens/users_list.dart';
 
 void main() => runApp(
       MaterialApp(
-        initialRoute: '/home',
+        initialRoute: '/login',
         routes: {
           '/home': (context) => HomeScreen(),
           '/splash': (context) => SplashScreen(),
           '/profile': (context) => ProfileScreen(),
           '/time_calc': (context) => TimeCalculatorScreen(),
           '/users_list': (context) => UsersListScreen(),
+          '/login': (context) => LoginScreen(),
         },
       ),
     );
---
+++ lib/screens/login/login_background.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class LoginBackground extends StatelessWidget {
+  final screenHeight;
+
+  LoginBackground({Key key, @required this.screenHeight}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: BottomShapedClipper(),
+      child: Container(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            color: Colors.white,
+            child: Image(
+              image: AssetImage('assets/background_pic.jpg'),
+            ),
+          ),
+        ),
+        height: screenHeight * .45,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.blueAccent,
+              Colors.blue[500],
+              Colors.blue[100],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomShapedClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Offset curveStart = Offset(0, size.height * .75);
+    Offset curveEnd = Offset(size.width, size.height * .75);
+
+    Path path = Path();
+    path.lineTo(curveStart.dx, curveStart.dy);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, curveEnd.dx, curveEnd.dy);
+    path.lineTo(size.width, 0.0);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
